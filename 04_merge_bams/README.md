8f72b9ea9a68537bf3ed70672baf502e2926a1f7
Morgan Sparks <60899634+morgan-sparks@users.noreply.github.com>
2023-07-11 16:07:07 -0600

    Add files via upload

--- 04_merge_bams/README.md
+++ 04_merge_bams/README.md
@@ -1,1 +1,3 @@
+These scripts are written out by the Rscipt that loops over file names and to write out a .sh file to submit to standby.
 
+The process the .sh files is to use picard-tools to merge bam files and samtools to write out .txt file with alignment/merge stats.
---
+++ 04_merge_bams/mergebams_STL_014.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=mergebams_STL_014
+#SBATCH -A standby
+#SBATCH -t 4:00:00
+#SBATCH -N 1
+#SBATCH -n 64
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=sparks35@purdue.edu
+
+module purge
+module load bioinfo
+module load picard-tools/2.20.8
+module load samtools/1.12
+
+PROJHOME=/scratch/bell/sparks35/GL_Pink_Salmon
+ADDRG=/scratch/bell/sparks35/GL_Pink_Salmon/data/seqs/aligned_reads_Ogor1.0//03_addedRG_bam/alignment_even
+MERGED=/scratch/bell/sparks35/GL_Pink_Salmon/data/seqs/aligned_reads_Ogor1.0/04_merged_bams/alignment_even
+
+
+java -Xmx124g -jar /group/bioinfo/apps/apps/picard-tools-2.9.0/picard.jar MergeSamFiles \
+I=$ADDRG/STL_014_run1_Ogor1.0_addedRG.bam \
+I=$ADDRG/STL_014_run2_Ogor1.0_addedRG.bam \
+SORT_ORDER=coordinate \
+O=$MERGED/STL_014_Ogor1.0_merged.bam
+
+samtools flagstat -@ 126 -O tsv $MERGED/STL_014_Ogor1.0_merged.bam > $MERGED/merged_flagstat/STL_014_rgroups_flagstat_out.txt
